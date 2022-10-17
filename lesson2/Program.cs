76027a4aa1d4ad0d0c43a1915e1aa62277074a56
Irina <migalina.i@icloud.com>
2022-10-17 15:21:02 +0300

    lesson2

---
+++ lesson2/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8 -> 30
+*/
+
+
+int EnterInt(string prompt)
+{
+    Console.Write(prompt);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int SumAllNumbs(int m, int n)
+{
+ int sum = 0;
+    if (m == n)
+    {
+        return m;
+    }
+    sum += n;
+    return SumAllNumbs(m, n - 1) + sum;
+}
+
+int numberM = EnterInt("Введите число M: ");
+int numberN = EnterInt("Введите число N: ");
+
+Console.WriteLine($"Cуммa натуральных элементов в промежутке от M до N: {SumAllNumbs(numberM, numberN)}");
